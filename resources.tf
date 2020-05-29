c8572a29b29672cef36249159319bf9d3656a307
Phillip Hocking <Phillip.Hocking@excelsiorwellness.org>
2020-05-29 00:04:39 -0700

    refactor for 0.12

--- resources.tf
+++ resources.tf
@@ -1,9 +1,9 @@
 #providers
 
 provider "aws" {
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
-  region     = "${var.region}"
+  access_key = var.access_key
+  secret_key = var.secret_key
+  region     = var.region
 }
 
 resource "aws_iam_user" "instanceManageUser" {
@@ -11,20 +11,18 @@ resource "aws_iam_user" "instanceManageUser" {
 }
 
 resource "aws_iam_access_key" "instanceManageUser" {
-  user = "${aws_iam_user.instanceManageUser.name}"
+  user = aws_iam_user.instanceManageUser.name
 }
 
 data "template_file" "policy" {
   template = "${file("${path.module}/policy.json")}"
-
-  vars {
-    bucket_name = "dummy_bucket"
-  }
+  bucket_name = "MyBucket"
+  
 }
 
 resource "aws_iam_user_policy" "instanceManageUser_assume_role" {
-  name = "InstanceManagePolicy"
-  user = "${aws_iam_user.instanceManageUser.name}"
+  name = InstanceManagePolicy
+  user = aws_iam_user.instanceManageUser.name
 
-  policy = "${data.template_file.policy.rendered}"
+  policy = data.template_file.policy.rendered
 }
--- variables.tf
+++
@@ -1,7 +0,0 @@
-# Variables
-
-variable "access_key" {}
-variable "secret_key" {}
-variable "region" {
-  default = "us-east-2"
-}
